b24a16f55f925f4675640f3beed37666200fde7b
admin <abilmansuradilzan138@gmail.com>
2026-02-08 19:49:39 +0500

    Initial commit

---
+++ src/main/java/org/example/endterm/EndtermApplication.java
@@ -0,0 +1,13 @@
+package org.example.endterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndtermApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndtermApplication.class, args);
+    }
+
+}
